7cba26eb1397a0ebb4aaaa07c759a17844a01b51
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-10-15 21:47:49 -0400

    add response to client connection with status code

--- app/main.js
+++ app/main.js
@@ -1,14 +1,25 @@
 const net = require("net");
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-console.log("Logs from your program will appear here!");
+const PORT = 4221;
 
-// Uncomment this to pass the first stage
+// create a new TCP server.
+// When a client connects (determined from server.listen), node creates a socket object to represent a connection to that specific client. Node calls the provided callback with the socket.
+// Inside the callback, when a client connects, the inner callback is run
 const server = net.createServer((socket) => {
+  console.log("Client connected!");
+
+  // the inner callback registers a listener - when the client closes the connection, call these methods on the socket
   socket.on("close", () => {
     socket.end();
     server.close();
   });
+
+  // listen for data from the client
+  socket.on("data", (data) => {
+    console.log("Received data from the client:", data.toString()); // convert buffer to string, by default interpret it as UTF-8 encoded
+    socket.write("HTTP/1.1 200 OK\r\n\r\n"); // send data back to the client
+    socket.end();
+  });
 });
 
-server.listen(4221, "localhost");
+server.listen(PORT, "localhost"); // node sets up listeners for connections at the OS level
