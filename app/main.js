f04ca2a4d625b92572dafa81ab4358b7dce1d553
agrawalishaan <113543798+agrawalishaan@users.noreply.github.com>
2023-10-15 22:40:05 -0400

    add route for responding to get user-agent route

--- app/main.js
+++ app/main.js
@@ -3,6 +3,7 @@ const net = require("net");
 const PORT = 4221;
 
 const ECHO_ROUTE = "/echo";
+const USER_AGENT_ROUTE = "/user-agent";
 
 // create a new TCP server.
 // When a client connects (determined from server.listen), node creates a socket object to represent a connection to that specific client. Node calls the provided callback with the socket.
@@ -37,6 +38,17 @@ const server = net.createServer((socket) => {
           "Content-Type: text/plain\r\n" +
           `Content-Length: ${message.length}\r\n\r\n` +
           `${message}` // send the header and body, delimited with \r\n\r\n
+      );
+    } else if (path.startsWith(USER_AGENT_ROUTE)) {
+      const hostLine = requestLines[1];
+      const userAgentLine = requestLines[2];
+      const userAgentParts = userAgentLine.split(" ");
+      const userAgent = userAgentParts[1];
+      socket.write(
+        "HTTP/1.1 200 OK\r\n" +
+          "Content-Type: text/plain\r\n" +
+          `Content-Length: ${userAgent.length}\r\n\r\n` +
+          `${userAgent}`
       );
     } else {
       socket.write("HTTP/1.1 404 Not Found\r\n\r\n");
